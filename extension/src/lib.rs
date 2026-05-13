503a84b11a123cfe03f9b2db124737adde9fbf21
Eric S <eric@laurila.se>
2026-05-13 14:01:33 +0200

    Updates server path to absolute path

--- extension/src/lib.rs
+++ extension/src/lib.rs
@@ -1,3 +1,4 @@
+use std::env;
 use zed_extension_api::{self as zed, Result};
 
 const SERVER_PACKAGE: &str = "ansible-lint-win";
@@ -21,12 +22,18 @@ impl zed::Extension for AnsibleLintWinExtension {
             zed::npm_install_package(SERVER_PACKAGE, &latest)?;
         }
 
-        let node = zed::node_binary_path()?;
+        let cwd = env::current_dir()
+            .map_err(|e| format!("failed to read extension working directory: {e}"))?;
+        let server_path = cwd
+            .join("node_modules")
+            .join(SERVER_PACKAGE)
+            .join("dist")
+            .join("server.js");
 
         Ok(zed::Command {
-            command: node,
+            command: zed::node_binary_path()?,
             args: vec![
-                format!("node_modules/{}/dist/server.js", SERVER_PACKAGE),
+                server_path.to_string_lossy().into_owned(),
                 "--stdio".to_string(),
             ],
             env: Default::default(),
